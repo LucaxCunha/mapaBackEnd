b8e60a0415f3d84dea0449ecdbb3a42ed423176f
LucaxCunha <104803990+LucaxCunha@users.noreply.github.com>
2022-08-09 22:58:06 -0300

    Add files via upload

---
+++ dados/produtos.php
@@ -0,0 +1,10 @@
+<?php
+    $produto = [
+        ['id' => 1,'nome' => 'Marmita Executiva','valor' => 28,00, 'tipo' => 'Marmita', 'descricao' => 'Arroz, feijão , macarrão , 2 tipos de carne ', 'imagem' => 'img mapa php\levinha.png'],
+        ['id' => 2,'nome' => 'Carne de Panela','valor' => 23,00, 'tipo' => 'Marmita','descricao' => 'Arroz , feijão branco , aipim , farofa , carne de panela', 'imagem' => 'img mapa php\quentinha.png'],
+        ['id' => 3,'nome' => 'Popular','valor' => 18,00, 'tipo' => 'Marmita','descricao' => 'Arroz, feijão , batata frita , linguiça', 'imagem' => 'img mapa php\OIP (4).jpg'],
+        ['id' => 4,'nome' => 'Vegana','valor' => 20,00, 'tipo' => 'Marmita','descricao' => 'Arroz integral, abobrinha , carne de soja', 'imagem' => 'img mapa php\vegetariana.png'],
+        ['id' => 5,'nome' => 'Sopa','valor' => 25.00, 'tipo' => 'Marmita','descricao' => 'Sopa de macarrão com legumes e carne ', 'imagem' => 'img mapa php\OIP.jpg']
+        
+    ];   
+?>
